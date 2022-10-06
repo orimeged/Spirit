1182a540053a61542e509c6e647d4d7c4678376b
orime <orimeged4>
2022-10-06 21:15:52 +0300

    create simpel server

---
+++ spirit ball/ball.py
@@ -0,0 +1,13 @@
+import socket
+server_socket = socket.socket()
+server_socket.bind(("0.0.0.0", 1337))
+server_socket.listen()
+print("Server is up and running")
+(client_socket, client_address) = server_socket.accept()
+print("Client connected")
+data = client_socket.recv(1024).decode()
+print("Client sent: " + str(data))
+reply = "Hello " + str(data)
+client_socket.send(reply.encode())
+client_socket.close()
+server_socket.close()
